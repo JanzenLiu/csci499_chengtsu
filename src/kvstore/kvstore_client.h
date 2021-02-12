1ce26b90765cfdf3496220adad9c9efab2518c2c
JanzenLiu <chengtsung_liu@yahoo.com>
2021-02-12 13:05:25 -0800

    Add kvstore gRPC client

---
+++ src/kvstore/kvstore_client.h
@@ -0,0 +1,31 @@
+#ifndef CSCI499_CHENGTSU_KVSTORE_CLIENT_H
+#define CSCI499_CHENGTSU_KVSTORE_CLIENT_H
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <grpcpp/grpcpp.h>
+
+#include "kvstore.grpc.pb.h"
+
+class KVStoreClient {
+ public:
+  KVStoreClient(std::shared_ptr<grpc::Channel> channel)
+      : stub_(kvstore::KeyValueStore::NewStub(channel)) {}
+
+  // Adds a value under the key, and returns true/false
+  // if the put was successful/failed.
+  bool Put(std::string key, std::string value);
+
+  // Returns all previously stored values under the key.
+  std::vector<std::string> Get(std::string key);
+
+  // Deletes all previously stored values under the key.
+  void Remove(std::string key);
+
+ private:
+  std::unique_ptr<kvstore::KeyValueStore::Stub> stub_;
+};
+
+#endif //CSCI499_CHENGTSU_KVSTORE_CLIENT_H
