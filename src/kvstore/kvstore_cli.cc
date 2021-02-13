cbf598bdcbdd34fe76ef2ee019877ca5f276e952
JanzenLiu <chengtsung_liu@yahoo.com>
2021-02-12 21:06:30 -0800

    Add client command line tool for the KVStore service.

---
+++ src/kvstore/kvstore_cli.cc
@@ -0,0 +1,90 @@
+#include "kvstore_client.h"
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <gflags/gflags.h>
+#include <grpcpp/grpcpp.h>
+
+using std::cout;
+using std::endl;
+using std::ostream;
+using std::string;
+using std::vector;
+
+// Outputs a vector to an output stream.
+template<typename T>
+ostream& operator<<(ostream& os, const vector<T>& vec) {
+  os << "[ ";
+  for (auto& elem : vec) {
+    os << elem << " ";
+  }
+  os << "]";
+  return os;
+}
+
+// Parses the command input by user in the CLI.
+vector<string> ParseCommand(const string& command) {
+  vector<string> tokens;
+  size_t start = 0;  // Begin index of the next search.
+  while (start < command.length()) {
+    // Simply split tokens by the space character.
+    size_t pos = command.find(" ", start);
+    if (pos == string::npos) { pos = command.length(); }
+    if (pos > start) {
+      string token = command.substr(start, pos - start);
+      tokens.push_back(token);
+    }
+    start = pos + 1;
+  }
+  return tokens;
+}
+
+DEFINE_int32(port, 50001, "Port number for the kvstore GRPC interface to use.");
+
+// Runs client command line tool for the KVStore gRPC service.
+int main(int argc, char** argv) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  // Validate command line arguments.
+  if (FLAGS_port < 0 or FLAGS_port > 65535) {
+    cout << "Invalid port number: " << FLAGS_port << "." << endl;
+  }
+
+  // Instantiate the client. It requires a channel, out of which the actual RPCs
+  // are created. This channel models a connection to an endpoint (in this case,
+  // localhost at port 50051). We indicate that the channel isn't authenticated
+  // (use of InsecureChannelCredentials()).
+  string target_str = "localhost:" + std::to_string(FLAGS_port);
+  auto channel = grpc::CreateChannel(
+      target_str, grpc::InsecureChannelCredentials());
+  KVStoreClient client(channel);
+
+  auto promptInvalid = [](){ cout << "Invalid command." << endl; };
+
+  // Loop until system interrupt or command to exit.
+  for (string line; std::getline(std::cin, line);) {
+    auto tokens = ParseCommand(line);
+    if (tokens.empty()) {
+      promptInvalid();
+      continue;
+    }
+    string op = tokens[0]; // Operation: put/get/remove/exit.
+    if (op == "put" && tokens.size() >= 3) {
+      client.Put(tokens[1], tokens[2]);
+    } else if (op == "get" && tokens.size() >= 2) {
+      cout << client.Get(tokens[1]) << endl;
+    } else if (op == "delete" && tokens.size() >= 2) {
+      client.Remove(tokens[1]);
+    } else if (op == "exit") {
+      break;
+    } else {
+      promptInvalid();
+      continue;
+    }
+  }
+
+  return 0;
+}
